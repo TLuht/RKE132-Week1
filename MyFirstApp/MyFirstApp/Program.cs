edf030a6ed6ae2d90e208761594e5d3df2ecadc3
TLuht <thomas.luht@tktk.ee>
2023-12-19 23:04:19 +0200

    create new app

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,6 @@
+﻿//palub kasutajal kirjutada oma nimi
+//kirjutab tervituse koos nimega
+
+Console.WriteLine("Enter your name:"); //väljund
+string userName = Console.ReadLine(); //sisend
+Console.WriteLine($"Hello, {userName}!"); //väljund
